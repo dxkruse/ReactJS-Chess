7d86ce4509aaae30eb4fc9d7f3db5c0100ed7680
dxkruse <dxkruse@gmail.com>
2021-04-07 02:08:40 -0500

    added pawn logic

--- chess-app/src/components/Chessboard/Chessboard.tsx
+++ chess-app/src/components/Chessboard/Chessboard.tsx
@@ -3,40 +3,59 @@ import { useRef, useState } from 'react';
 import React from 'react';
 import Tile from '../Tile/Tile';
 import './Chessboard.css';
+import Referee from '../Referee/Referee';
+
 
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"]
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"]
 
 
-interface Piece {
+export interface Piece {
     image: string;
     x: number;
     y: number;
+    type: PieceType;
+    team: TeamType;
+}
+
+export enum PieceType {
+    PAWN,
+    BISHOP,
+    KNIGHT,
+    ROOK,
+    QUEEN,
+    KING
+}
+
+export enum TeamType {
+    OPPONENT,
+    PLAYER
 }
 
 const initialBoardState: Piece[] = [];
 
 for (let p=0; p < 2; p++) {
+    const teamType = (p === 0) ? TeamType.OPPONENT : TeamType.PLAYER;
     const type = p === 0 ? "b" : "w";
-    const y = p === 0 ? 7 : 0;
+    const y = (teamType === TeamType.OPPONENT) ? 7 : 0;
 
-    initialBoardState.push({image: `assets/images/rook_${type}.png`, x: 0, y: y})
-    initialBoardState.push({image: `assets/images/rook_${type}.png`, x: 7, y: y})
-    initialBoardState.push({image: `assets/images/knight_${type}.png`, x: 1, y: y})
-    initialBoardState.push({image: `assets/images/knight_${type}.png`, x: 6, y: y})
-    initialBoardState.push({image: `assets/images/bishop_${type}.png`, x: 2, y: y})
-    initialBoardState.push({image: `assets/images/bishop_${type}.png`, x: 5, y: y})
-    initialBoardState.push({image: `assets/images/queen_${type}.png`, x: 3, y: y})
-    initialBoardState.push({image: `assets/images/king_${type}.png`, x: 4, y: y})
+    initialBoardState.push({image: `assets/images/rook_${type}.png`, x: 0, y: y, type: PieceType.ROOK, team: teamType});
+    initialBoardState.push({image: `assets/images/rook_${type}.png`, x: 7, y: y, type: PieceType.ROOK, team: teamType});
+    initialBoardState.push({image: `assets/images/knight_${type}.png`, x: 1, y: y, type: PieceType.KNIGHT, team: teamType});
+    initialBoardState.push({image: `assets/images/knight_${type}.png`, x: 6, y: y, type: PieceType.KNIGHT, team: teamType});
+    initialBoardState.push({image: `assets/images/bishop_${type}.png`, x: 2, y: y, type: PieceType.BISHOP, team: teamType});
+    initialBoardState.push({image: `assets/images/bishop_${type}.png`, x: 5, y: y, type: PieceType.BISHOP, team: teamType});
+    initialBoardState.push({image: `assets/images/queen_${type}.png`, x: 3, y: y, type: PieceType.QUEEN, team: teamType});
+    initialBoardState.push({image: `assets/images/king_${type}.png`, x: 4, y: y, type: PieceType.KING, team: teamType});
 
 }
 
 for (let i=0; i < 8; i++) {
-    initialBoardState.push({image: "assets/images/pawn_b.png", x: i, y: 6})
+    initialBoardState.push({image: "assets/images/pawn_b.png", x: i, y: 6, type: PieceType.PAWN, team: TeamType.OPPONENT});
 }
 
 for (let i=0; i < 8; i++) {
-    initialBoardState.push({image: "assets/images/pawn_w.png", x: i, y: 1})
+    initialBoardState.push({image: "assets/images/pawn_w.png", x: i, y: 1, type: PieceType.PAWN, team: TeamType.PLAYER});
 }
 
 export default function Chessboard() {
@@ -45,10 +64,13 @@ export default function Chessboard() {
     const [gridY, setGridY] = useState(0);
     const [pieces, setPieces] = useState<Piece[]>(initialBoardState);
     const chessboardRef = useRef<HTMLDivElement>(null);
+    const referee = new Referee();
 
     function grabPiece(e: React.MouseEvent) {
         const chessboard = chessboardRef.current;
         const element = e.target as HTMLElement;
+
+
         if(element.classList.contains("chess-piece") && chessboard) {
 
             setGridX(Math.floor((e.clientX - chessboard.offsetLeft) / 100));
@@ -100,11 +122,22 @@ export default function Chessboard() {
             
             const x = Math.floor((e.clientX - chessboard.offsetLeft) / 100);
             const y = Math.floor((e.clientY - chessboard.offsetTop - 800) / -100);
+
             setPieces((value) => {
                 const pieces = value.map(p => {
                     if(p.x === gridX && p.y === gridY) {
-                        p.x = x;
-                        p.y = y;
+                        const validMove = referee.isValidMove(gridX, gridY, x, y, p.type, p.team, value);
+
+                        if (validMove) {
+                            p.x = x;
+                            p.y = y;                         
+                        } else {
+                            activePiece.style.position = 'relative';
+                            activePiece.style.removeProperty('top');
+                            activePiece.style.removeProperty('left');
+                            // p.x = gridX;
+                            // p.y = gridY;
+                        }
                     }
                     return p;
                 })
---
+++ chess-app/src/components/Referee/Referee.ts
@@ -0,0 +1,41 @@
+import { PieceType, TeamType, Piece } from "../Chessboard/Chessboard";
+
+export default class Referee {
+
+    tileIsOccupied(x:number, y:number, boardState: Piece[]): boolean {
+        console.log("checking if tile is occupied");
+        return true;
+    }
+
+    isValidMove(x0: number, y0: number, x1: number, y1: number, type: PieceType, team: TeamType, boardState: Piece[]) {
+        console.log("ref is checking move");
+        console.log(`previous location: ${x0}, ${y0}`);
+        console.log(`current location: ${x1}, ${y1}`);
+        console.log(`piece type: ${type}`);
+        console.log(`team: ${team}`);
+
+        if(type === PieceType.PAWN) {
+            if(team === TeamType.PLAYER) {
+                if(y0 === 1) {
+                    if(x0 === x1 && (y1 - y0 === 1 || y1 - y0 === 2)) {
+                        if (!this.tileIsOccupied(x1, y1, boardState)) {
+                            return true;
+                        }
+                        return true;
+                    } 
+                } else if (x0 === x1 && y1 - y0 === 1) {
+                    return true;
+                }
+            } else {
+                if(y0 === 6) {
+                    if(x0 === x1 && (y1 - y0 === -1 || y1 - y0 === -2)) {
+                        return true;
+                    }
+                } else if (x0 === x1 && y1 - y0 === -1) {
+                    return true;
+                }
+        //return false;
+            }
+        }
+    }
+}
